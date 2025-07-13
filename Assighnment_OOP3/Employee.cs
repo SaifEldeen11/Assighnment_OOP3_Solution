464aee5e50fa00ee674d6c3cca384e2b01fabf65
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-07-13 21:46:58 +0300

    Add project files.

---
+++ Assighnment_OOP3/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assighnment_OOP3
+{
+    #region Employee
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public double Salary { get; set; }
+
+        public string HiringDate { get; set; } = string.Empty;
+
+        public securityLeve securityLeve { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name},Salary: {Salary:c},HiringDate: {HiringDate} , ,Security Level: {securityLeve}";
+        }
+
+    }
+    [Flags]
+    enum securityLeve : byte
+    {
+        guest = 1,
+        Developer = 2,
+        secretary = 4,
+        DBA = 16
+    }
+    [Flags]
+    enum Gender : byte
+    {
+        M = 1,
+        F = 2,
+        m = M,
+        f = F,
+    }
+    #endregion
+
+    #region HiringDate
+
+    #endregion
+}
---
+++ Assighnment_OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assighnment_OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Employee e1 = new Employee();
+            //e1.Id = 1;
+            //e1.Name = "Saif";
+            //e1.Salary = 1000;
+            //e1.HiringDate = "2023-10-01";
+            //e1.securityLeve = securityLeve.Developer;
+            //Console.WriteLine(e1.ToString());
+            #endregion
+
+            #region Q2
+
+            #endregion
+        }
+    }
+}
