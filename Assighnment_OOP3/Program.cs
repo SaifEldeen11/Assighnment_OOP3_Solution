17bc344321a4122cbd7f1e01849fce340603ae55
SaifAhmed11 <saifeldeenahmed942@gmail.com>
2025-07-13 23:29:17 +0300

    DONE

--- Assighnment_OOP3/Program.cs
+++ Assighnment_OOP3/Program.cs
@@ -4,18 +4,83 @@
     {
         static void Main(string[] args)
         {
-            #region Q1
-            //Employee e1 = new Employee();
-            //e1.Id = 1;
-            //e1.Name = "Saif";
-            //e1.Salary = 1000;
-            //e1.HiringDate = "2023-10-01";
-            //e1.securityLeve = securityLeve.Developer;
-            //Console.WriteLine(e1.ToString());
+            #region Q1  
+            //Employee e1 = new Employee();  
+            //e1.Id = 1;  
+            //e1.Name = "Saif";  
+            //e1.Salary = 1000;  
+            //e1.HiringDate = "2023-10-01";  
+            //e1.AccessLevel = Employee.SecurityLevel.Developer;  
+            //Console.WriteLine(e1.ToString());  
             #endregion
 
-            #region Q2
+            #region Q2  
+            //HiringDate h1 = new HiringDate();  
+            //h1.Years = "2022";  
+            //h1.Months = "14";  
+            //h1.Days = "02";  
+            //Console.WriteLine(h1.ToString());  
+            #endregion
 
+            #region Q3  
+            //Employee[] e1 = new Employee[3];  
+            //e1[0] = new Employee  
+            //{  
+            //    Id = 12,  
+            //    AccessLevel = Employee.SecurityLevel.DBA  
+            //};  
+
+            //e1[1] = new Employee  
+            //{  
+            //    Id = 13,  
+            //    AccessLevel = Employee.SecurityLevel.Guest  
+            //};  
+
+            //e1[2] = new Employee  
+            //{  
+            //    Id = 14,  
+            //    AccessLevel = Employee.SecurityLevel.Officer  
+            //};  
+            #endregion
+
+            #region Q4
+            //Employee[] EmpArr = new Employee[3];
+            //EmpArr[0] = new Employee()
+            //{
+            //    Id = 1,
+            //    HiringDate = "2023-10-03",
+            //};
+            //EmpArr[1] = new Employee()
+            //{
+            //    Id = 2,
+            //    HiringDate = "2023-10-02",
+            //};
+
+            //EmpArr[2] = new Employee()
+            //{
+            //    Id = 3,
+            //    HiringDate = "2023-10-01",
+            //};
+
+            //Array.Sort(EmpArr, (a, b) => DateTime.Parse(a.HiringDate).CompareTo(DateTime.Parse(b.HiringDate)));
+            //foreach (var emp in EmpArr)
+            //{
+            //    Console.WriteLine(emp);
+            //}
+            #endregion
+
+            #region Q5
+//            Book[] books = new Book[]
+//{
+//            new EBook { Title = "C# OOP", Author = "Hamada", ISBN = "123-A", FileSize = 500 },
+//            new PrintedBook { Title = "Clean Code" , Author = "Saif", ISBN = "456-B", PageCount = 310 }
+//};
+
+//            foreach (var book in books)
+//            {
+//                book.DisplayInfo();
+//                Console.WriteLine("------------");
+//            }
             #endregion
         }
     }
